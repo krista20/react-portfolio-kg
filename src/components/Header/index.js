ece53c0801cbace3900079b59659ef5bbe6a2ea8
Rush <kriscookies20@gmail.com>
2022-08-01 02:15:48 -0700

    added name in nav, and created a link to link back to main page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,28 +8,32 @@ function Header(props) {
 
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div>
-                <ul className="nav container-fluid">
+                <ul className="nav container-fluid mx-5">
+                    {/* Name & click to route to main page */}
+                    <a href="/" rel="mainpage" style={{textDecoration: "none"}}>
+                    <h1 className="text-white">Krista Garcia</h1>
+                    </a>
                 
                     {/* about me */}
-                    <li className="nav-item">
+                    <li className="nav-item mx-5">
                         <span className="btn nav-link active text-white" style={{border:"none"}} onClick={() => {
                             props.setCurrentPage("About")
                         }}>About</span>
                     </li>
                     {/* portfolio */}
-                    <li className="nav-item">
+                    <li className="nav-item mx-5">
                         <span className="btn nav-link active text-white" style={{border:"none"}} onClick={() => {
                             props.setCurrentPage("Portfolio")
                         }}>Portfolio</span>
                     </li>
                     {/* Resume */}
-                    <li className="nav-item">
+                    <li className="nav-item mx-5">
                         <span className="btn nav-link active text-white" style={{border:"none"}} onClick={() => {
                             props.setCurrentPage("Resume")
                         }}>Resume</span>
                     </li>
                     {/* Contact */}
-                    <li className="nav-item">
+                    <li className="nav-item mx-5">
                         <span className="btn nav-link active text-white" style={{border:"none"}} onClick={() => {
                             props.setCurrentPage("Contact")
                         }}>Contact</span>
